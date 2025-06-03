ebc56adee50c28b06ada7337f38acd62a05e11a3
All-true-ism <sjfalzar@yahoo.com>
2025-06-03 07:07:11 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
+}
+
+function startLive() {
+  alert("Pretend we're going live... and you look amazing doing it.");
+}
